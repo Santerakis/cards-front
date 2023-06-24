da45e7bd398cce2f98bdc5e60493d2792fd29aa4
Sergey <santerakis@gmail.com>
2023-06-24 22:04:07 +0300

    step

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -9,26 +9,23 @@ const register = createAppAsyncThunk<any, ArgRegisterType>("auth/registerThunk",
   const { dispatch, rejectWithValue } = thunkAPI
 
   // return authApi.register(arg).then(res => {
-  //   dispatch(authActions.setString({ q: "privettt" }))
   //   return 'www'
   // })
   //   .catch(e => {
   //     const err = e.response.data.error
-  //     // dispatch(appActions.setIsLoading({isLoading: false}))
   //     debugger
   //     // dispatch(authActions.setString({q: err}))
-  //     return rejectWithValue({ q: 'samurai' })
+  //     return rejectWithValue({ q: 'catch_samurai' })
   //   })
 
-  try {
+  // try {
     const res = await authApi.register(arg)
     return res
-  } catch (e: any) {
-    const err = e.response.data.error
-    dispatch(appActions.setIsLoading({isLoading: false}))
-    console.error('reg_error: ', e)
-    return rejectWithValue({ q: 'samurai' })
-  }
+  // } catch (e: any) {
+  //   const err = e.response.data.error
+   //   console.error('reg_error: ', e)
+  //   return rejectWithValue({ q: err })
+  // }
 
 })
 const login = createAsyncThunk("auth/loginThunk", (arg: ArgLoginType, thunkAPI) => {
@@ -60,8 +57,8 @@ const slice = createSlice({
     })
       .addCase(register.rejected, (state, action) => {
       debugger
-      // state.q = 'hello'
-      state.q = action.payload.q
+      state.q = 'hello_rejected'
+      // state.q = action.payload.q
     })
       .addCase(register.fulfilled, (state, action) => {
       debugger
